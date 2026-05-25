f215454124cba93b0b71fac734e79e85e8d584f1
Breno Freguglia <freguglia.breno@gmail.com>
2026-05-25 20:27:11 -0300

    fix: Ajuste na autenticação do sistema, correção na parte do database e ajuste no usuario admin

--- Biopower/utils/database.js
+++ Biopower/utils/database.js
@@ -12,10 +12,9 @@ class Database {
 
   constructor() {
     this.#conexao = mysql.createPool({
-      host: "localhost", //endereço do nosso banco de dados na nuvem
-      database: "bio_sys_db", //a database de cada um de vocês possui a nomenclatura PFS1_(RA)
-      user: "root", // usuario e senha de cada um de vocês é o RA
-      password: "root", // usuario e senha de cada um de vocês é o RA
+      host: "localhost", 
+      database: "bio_sys_db",
+      user: "root", 
       idleTimeout: 30000,
       connectionLimit: 50,
     });
